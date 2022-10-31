844e7e2084acb29783489c98ab3814848f396f8b
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-10-31 21:44:54 +0330

    Add project files.

---
+++ RepositoryUnitOfWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositoryUnitOfWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
